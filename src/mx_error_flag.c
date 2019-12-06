edc9bb9531cbd5b3a0361adc2f5c2216b7ba3f71
Oleksandr Arnopolin <oarnopolin@e2r9p3.unit.ua>
2019-12-06 14:57:19 +0200

    update mx_error_flag

--- src/mx_error_flag.c
+++ src/mx_error_flag.c
@@ -4,27 +4,27 @@ void mx_error_flag(int argc, char **flags){
     char *us_f = "ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1";
     int count = 0;
     int flag = 0;
-    int j = 0;
+    int j = 1;
 
     for (int i = 1; i < argc; i++) {
-        count = 0;
+        j = 1;
         if (flags[i][0] != '-'){
             mx_error_dir(flags, i);
         }  
-        while (us_f[count] != '\0'){
-            j = 0;
+        while (flags[i][j] != '\0'){
+            count = 0;
             flag = 0;
-            while (flags[i][j] != '\0') {
+            while (us_f[count] != '\0') {
                 if (us_f[count] == flags[i][j]) {
                     flag = 1;
                     break;
                 }
-                j++;
+                count++;
             }
             if (flag == 0) {
                 mx_print_error_flag(i, j,flags);
             }
-            count++;
+            j++;
         }
     }
 }
